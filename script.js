dacc29d0b89038365c5823a3f1c37b3750f839cc
galvinsusilo <galvinsusilo@gmail.com>
2025-09-25 04:56:19 +0700

    First commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const slides = document.querySelectorAll(".figma-slider .slide");
+const prevBtn = document.querySelector(".figma-slider .prev");
+const nextBtn = document.querySelector(".figma-slider .next");
+
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle("active", i === index);
+  });
+}
+
+nextBtn.addEventListener("click", () => {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+});
+
+prevBtn.addEventListener("click", () => {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+});
